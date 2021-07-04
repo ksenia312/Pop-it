2ed7c5698452bd407710eb30d26309b20dcf4193
ksenia312 <ksynikitina.02@mail.ru>
2021-07-04 21:02:44 +0300

    new button

--- move.js
+++ move.js
@@ -1,8 +1,22 @@
-window.tap = function(count) {
+window.mov = function(count) {
     count=count.toString()
     if (document.getElementById(count).getAttribute('class') === 'top') {
         document.getElementById(count).setAttribute('class', 'bottom');
-    } else {
+    }
+    else {
         document.getElementById(count).setAttribute('class', 'top');
+    }
+}
+
+window.tap = function(count) {
+    if (count===-1) {
+        let i = 1;
+        while (i<=36) {
+            mov(i)
+            i++     
+        }
+    }
+    else {
+        mov(count)
     }
 }
