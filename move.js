370f457431c10b9bcdcf330db7c62308c72398d4
ksenia312 <nikitina3619@gmail.com>
2023-07-12 12:27:13 +0200

    feat: update styles and icon

--- move.js
+++ move.js
@@ -1,32 +1,46 @@
-window.mov = function(count) {
-    count=count.toString()
+window.mov = function (count, sound = true) {
+    count = count.toString()
     if (document.getElementById(count).getAttribute('class') === 'top') {
         document.getElementById(count).setAttribute('class', 'bottom');
-        let audio = new Audio();
-        audio.src = 'audio.wav';
-        audio.autoplay = true;
-    }
-    else {
+        if (sound) {
+            let audio = new Audio();
+            audio.src = 'audio.wav';
+            audio.autoplay = true;
+        }
+    } else {
         document.getElementById(count).setAttribute('class', 'top');
     }
 }
-window.update = function(count) {
-    count=count.toString()
+window.update = function (count) {
+    count = count.toString()
     document.getElementById(count).setAttribute('class', 'top');
 }
-window.tap = function(count) {
-    if (count===-1) {
+window.tap = function (count) {
+    if (count === -1) {
+        const container = document.querySelector("#container")
+        container.classList.add('rotating')
+
+        setTimeout(() => {
+            container.classList.remove('rotating')
+        }, 1000);
+
+        setTimeout(() => {
+            let i = 1;
+            while (i <= 36) {
+                mov(i, false);
+                i++
+            }
+        }, 500);
+
+
+    } else if (count === -2) {
 
         let i = 1;
-        while (i<=36) {mov(i); i++}
-    }
-    else if (count === -2) {
-
-        let i = 1;
-        while (i<=36) {update(i); i++}
-    }
-    else {
-
+        while (i <= 36) {
+            update(i);
+            i++
+        }
+    } else {
         mov(count)
     }
 }
